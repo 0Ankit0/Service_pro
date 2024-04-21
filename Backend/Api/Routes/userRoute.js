f6bbe0bd5ad33bb33e4bc113b60cb195cf7c44c8
0ankit0 <ankitpaudel.1373@gmail.com>
2024-04-21 18:27:39 +0545

    changed the ratelimit value

--- Backend/Api/Routes/userRoute.js
+++ Backend/Api/Routes/userRoute.js
@@ -6,7 +6,7 @@ import rateLimit from "express-rate-limit";
 const userRouter = Router();
 
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
+    windowMs: 1 * 60 * 1000, // 15 minutes
     max: 5, // limit each IP to 5 requests per windowMs
     handler: function (req, res, /*next*/) {
         const retryAfter = Math.ceil(res.getHeaders()['retry-after'] / 60); // Get the Retry-After header and convert it to minutes
