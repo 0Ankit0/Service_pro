eaf28c5d43af2fe745f225b624fb4b71aca4a1b0
0ankit0 <ankitpaudel.1373@gmail.com>
2024-04-22 18:21:48 +0545

    added post method to the category route

--- Backend/Api/Routes/CategoryRoute.js
+++ Backend/Api/Routes/CategoryRoute.js
@@ -10,4 +10,13 @@ CategoryRouter.get("/", async (req, res) => {
         return res.status(500).json({ error: error.message });
     }
 });
+
+CategoryRouter.post("/add", async (req, res) => {
+    try {
+        const category = await Category.create(req.body);
+        return res.status(201).json({ category });
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+});
 export default CategoryRouter;
