71edb00ab47687247e8d0b14df93c045b6514d50
0ankit0 <ankitpaudel.1373@gmail.com>
2024-04-28 07:05:43 +0545

    changed success status to 200 in category

--- Api/Routes/CategoryRoute.js
+++ Api/Routes/CategoryRoute.js
@@ -15,7 +15,7 @@ CategoryRouter.get("/", async (req, res) => {
 CategoryRouter.post("/add", async (req, res) => {
     try {
         const category = await Category.create(req.body);
-        return res.status(201).json({ category });
+        return res.status(200).json({ category });
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
@@ -27,7 +27,7 @@ CategoryRouter.put("/update/:id", async (req, res) => {
             { $set: { "Services": req.body.Services } },
             { new: true } // to return updated document
         );
-        return res.status(201).json({ message: "Category Updated Successfully" });
+        return res.status(200).json({ message: "Category Updated Successfully" });
     } catch (error) {
         return res.status(500).json({ error: error.message });
     }
