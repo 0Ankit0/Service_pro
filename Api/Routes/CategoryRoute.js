322f8e6ceffa3164e4e3dbedf58609202c46599f
0ankit0 <ankitpaudel.1373@gmail.com>
2024-05-01 05:58:20 +0545

    made the response format the same

--- Api/Routes/CategoryRoute.js
+++ Api/Routes/CategoryRoute.js
@@ -7,30 +7,30 @@ const CategoryRouter = Router();
 CategoryRouter.get("/", async (req, res) => {
     try {
         const categories = await Category.find().lean().exec();
-        return res.status(200).json({ categories });
+        return res.status(200).json({ message: "Categories fetched successfully", data: categories });
     } catch (error) {
-        return res.status(500).json({ error: error.message });
+        return res.status(400).json({ error: error.message });
     }
 });
 
 CategoryRouter.post("/add", protect, async (req, res) => {
     try {
         const category = await Category.create(req.body);
-        return res.status(200).json({ category });
+        return res.status(200).json({ message: "Category Added Successfully", data: category });
     } catch (error) {
-        return res.status(500).json({ error: error.message });
+        return res.status(400).json({ error: error.message });
     }
 });
 CategoryRouter.put("/update/:id", protect, async (req, res) => {
     try {
         const category = await Category.findByIdAndUpdate(
             req.params.id,
-            req.body,
+            { Services: req.body.Services },
             { new: true } // to return updated document
         );
         return res.status(200).json({ message: "Category Updated Successfully", data: category });
     } catch (error) {
-        return res.status(500).json({ error: error.message });
+        return res.status(400).json({ error: error.message });
     }
 });
 CategoryRouter.delete("/delete/:id", protect, async (req, res) => {
