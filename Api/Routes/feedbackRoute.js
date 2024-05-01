ea1af10575e468a016bf08467daa88143ec4c0b8
0ankit0 <ankitpaudel.1373@gmail.com>
2024-05-01 05:54:16 +0545

    added try catch and status on every request

--- Api/Routes/feedbackRoute.js
+++ Api/Routes/feedbackRoute.js
@@ -4,34 +4,40 @@ import { Router } from 'express';
 const feedbackRouter = Router();
 
 feedbackRouter.get('/', async (req, res) => {
-    var feedbacks = await Feedback.find({}).lean().exec();
-    res.json(feedbacks);
+    try {
+
+        var feedbacks = await Feedback.find({}).lean().exec();
+        res.status(200).json({ message: "Feedbacks fetched successfully", data: feedbacks });
+    } catch (error) {
+        res.status(400).json({ message: "Error occurred" });
+
+    }
 });
 
 feedbackRouter.post('/add', async (req, res) => {
     try {
         const feedback = await Feedback.create({ ...req.body, UserId: req.user.id });
-        res.json({ message: "Feedback added successfully" });
+        res.status(200).json({ message: "Feedback added successfully" });
     } catch (error) {
-        res.json({ message: "Error occurred" })
+        res.status(400).json({ message: "Error occurred" })
     }
 });
 
 feedbackRouter.delete('/delete/:id', async (req, res) => {
     try {
         const feedback = await Feedback.findByIdAndDelete(req.params.id);
-        res.json({ message: "Feedback deleted successfully" });
+        res.status(200).json({ message: "Feedback deleted successfully" });
     } catch (error) {
-        res.json({ message: "Error occurred" })
+        res.status(400).json({ message: "Error occurred" })
     }
 });
 
 feedbackRouter.put('/update/:id', async (req, res) => {
     try {
         const feedback = await Feedback.findByIdAndUpdate(req.params.id, req.body);
-        res.json({ message: "Feedback updated successfully" });
+        res.status(200).json({ message: "Feedback updated successfully" });
     } catch (error) {
-        res.json({ message: "Error occurred" })
+        res.status(400).json({ message: "Error occurred" })
     }
 });
 
