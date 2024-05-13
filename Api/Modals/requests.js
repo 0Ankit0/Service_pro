2eceeb2707fb72b14c34410ba512f86419033133
0ankit0 <ankitpaudel.1373@gmail.com>
2024-05-13 06:24:36 +0545

    chore: Update request schema with Image field and default Status value

--- Api/Modals/requests.js
+++ Api/Modals/requests.js
@@ -15,9 +15,14 @@ const requestSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
+    Image: { type: String },
     Date: { type: String, required: true },
     Time: { type: String, required: true },
-    Status: { type: String, required: true },
+    Status: {
+        type: String,
+        enum: ['pending', 'accepted', 'completed', 'cancelled'],
+        default: 'pending'
+    },
 }, { timestamps: true }
 )
 export const Request = mongoose.model('Request', requestSchema)
