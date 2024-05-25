833e9a01c9f2abeb74cee3d6d2920b7cd6d8c5f9
0ankit0 <ankitpaudel.1373@gmail.com>
2024-05-25 15:03:32 +0545

    chore: Refactor mailRoute.js to use nodemailer for sending emails and improve error handling

--- Api/Routes/mailRoute.js
+++ Api/Routes/mailRoute.js
@@ -32,53 +32,12 @@ mailRouter.post('/send', async (req, res) => {
                 res.status(500).send(error);
             } else {
                 console.log('Email sent: ' + info.response);
-                res.status(200).send('Email sent successfully ');
+                res.status(200).send({ message: 'Email sent successfully ' });
             }
         });
     } catch (error) {
-        req.status(500).send(error);
+        req.status(500).send({ message: error });
     }
 
-});
-mailRouter.post('/send-multiple', async (req, res) => {
-    const { EmailAddresses, Body } = req.body;
-
-    // Create a transporter using SMTP
-    // const transporter = nodemailer.createTransport({
-    //     host: 'smtp.example.com',
-    //     port: 587,
-    //     secure: false,
-    //     auth: {
-    //         user: 'your-email@example.com',
-    //         pass: 'your-password'
-    //     }
-    // });
-    //Or
-    //Create a transporter Using Gmail and ensure that you have enabled "Less secure apps" in your Google account settings
-    const transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'your_email@gmail.com',
-            pass: 'your_email_password',
-        },
-    });
-
-    // Define the mail options
-    const mailOptions = {
-        from: 'your-email@example.com',
-        to: EmailAddresses.join(', '), // Join the email addresses with a comma
-        subject: 'Mail Subject',
-        text: emailBody
-    };
-
-    try {
-        // Send the mail
-        await transporter.sendMail(mailOptions);
-        Mail.create({ Email: EmailAddresses, Body: emailBody });
-        res.send("Mail sent successfully");
-    } catch (error) {
-        console.error(error);
-        res.status(400).send("Failed to send mail");
-    }
 });
 export default mailRouter;
